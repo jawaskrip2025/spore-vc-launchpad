a875de35e198bc51f90701c19cf7877e3c9adb3b
Uut Budiarto <87194683+uutbudiarto2012@users.noreply.github.com>
2025-07-06 22:16:34 +0700

    Auth nonce (#4)
    * -
    
    * auth wallet dengan nonce

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const nextConfig = {
     PINATA_JWT: process.env.PINATA_JWT,
     PINATA_GATEWAY_URL: process.env.PINATA_GATEWAY_URL,
     W3AUTH_CLIENT_ID: process.env.W3AUTH_CLIENT_ID,
+    BASE_URL: process.env.BASE_URL,
   },
   images: {
     domains: [
--- src/components/wallet-button.tsx
+++ src/components/wallet-button.tsx
@@ -5,15 +5,37 @@ import { Icon } from './icon'
 import { Button } from './ui/button'
 import { WalletButtonConnected } from './wallet-button-connected'
 
+import { useRequestNonce, useVerifySignature } from '@/modules/auth/auth.query'
+import { Web3Provider } from '@ethersproject/providers'
 export default function WalletButton({ withText }: { withText?: boolean }) {
   const {
     connect,
     isConnected,
     loading: connecting,
   } = useWeb3AuthConnect();
+  const { mutate: requestNonce } = useRequestNonce()
+  const { mutate: verifySignature } = useVerifySignature()
 
   async function handleConnect() {
-    await connect()
+    
+    const web3Provider = await connect();
+    if (!web3Provider) return;
+    const result = await web3Provider.request({ method: 'eth_accounts' });
+    const accounts = Array.isArray(result) ? result as string[] : [];
+    const address = accounts[0];
+
+    requestNonce({ walletAddress: address }, {
+      onSuccess: async (data) => {
+        const nonce = data.data.nonce;
+        const provider = new Web3Provider(web3Provider)
+        const signer = provider.getSigner()
+        const signature = await signer.signMessage(nonce)
+        verifySignature({
+          walletAddress: address,
+          signature: signature
+        })
+      }
+    })
   }
   return (
     <div>
@@ -22,7 +44,7 @@ export default function WalletButton({ withText }: { withText?: boolean }) {
           isConnected ? (
             <WalletButtonConnected />
           ) : (
-              <>
+            <>
               <Button disabled={connecting} onClick={handleConnect} className='hidden md:flex'>
                 <Icon name='solar:wallet-2-bold' />
                 <p>Connect</p>
